b94da6e7ab02d6b0c14f95dba9c3e80f12bb57c8
jiyoung <aa991102@naver.com>
2025-07-18 15:47:36 +0900

    feat: AppLogo 컴포넌트 접근성 개선 및 사이즈 prop 추가

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -1,9 +1,15 @@
 import './AppLogo.css'
 
-export default function AppLogo() {
+interface AppLogoProps extends React.HTMLAttributes<HTMLDivElement> {
+  size?: 'sm' | 'base' | 'lg';
+}
+
+export default function AppLogo({ size = 'lg', ...props }: AppLogoProps) {
   return (
-    <div className="app-logo">
-      🚀 지락실 🚀
+    <div className={`app-logo font-${size}`} role="img" aria-label="지락실 로고" {...props}>
+      <span aria-hidden="true">🚀</span>
+      <span>지락실</span>
+      <span aria-hidden="true">🚀</span>
     </div>
   )
 }
