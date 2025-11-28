14bfcddae7826ec72aa48bc274bac12b8edf7739
jiyoung <aa991102@naver.com>
2025-11-28 15:06:50 +0900

    feat(common): 로컬 시간 문자열 변환 함수 추가
    - toLocalDateTimeString() 유틸 함수 추가
    - formatDateTime 개선: 포맷 문자열 인수로 받도록 수정. 기본값 설정

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,23 @@
 import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
+
 dayjs.locale('ko');
 
-export const formatDateTime = (date: string | Date) => {
-  return dayjs(date).format('YY년 MM월 DD일(ddd) A h:mm');
+/**
+ * 날짜를 다양한 포맷으로 변환하는 유틸
+ * 
+ * @param date - 날짜 문자열 또는 Date 객체
+ * @param format - dayjs 포맷 문자열 (기본: 화면 표시용)
+ * @returns 포맷팅된 문자열
+ */
+export const formatDateTime = (date: string | Date, format = 'YY년 MM월 DD일(ddd) A h:mm') => {
+  return dayjs(date).format(format);
+};
+
+/**
+ * 서버 LocalDateTime 저장용(KST 유지)
+ * 예: 2025-11-27T18:48:26
+ */
+export const toLocalDateTimeString = (date: string | Date) => {
+  return dayjs(date).format('YYYY-MM-DDTHH:mm:ss');
 };
